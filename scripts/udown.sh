ae69b2e464d24a3d83c2c55a134d59a8917df331
jhkim <jhkim@nexell.co.kr>
2018-06-06 13:53:14 +0900

    tools: udown: set download images type to arrary
    this patch set download images type to arrary
    
    Change-Id: I4521af6f9fa9745d79099c9d510a2c735f6c06c6
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- scripts/udown.sh
+++ scripts/udown.sh
@@ -2,33 +2,21 @@
 
 BASEDIR=$(cd "$(dirname "$0")" && pwd)
 RESULT=$BASEDIR/../../result
-WAITSEC=2
-
-BL1=$RESULT/nxp3220_bl1.bin.raw
-BL2=$RESULT/bl2-vtk.bin.raw
-SSS=$BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw
-BL32=$RESULT/bl32.bin.raw
-UBOOT=$RESULT/u-boot.bin.raw
-
 DOWNLOADER=$BASEDIR/../linux-usbdownloader/linux-usbdownloader
 
-echo "*** USB Download $BL1 ***"
-sudo $DOWNLOADER -t nxp3220 -f $BL1  -a 0xFFFF0000 -j 0xFFFF0000
-sleep $WAITSEC
+nxp3220=(
+	"-f $RESULT/nxp3220_bl1.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
+	"-f $RESULT/bl2-vtk.bin.raw -a 0xffff8000 -j 0xffff8000"
+	"-f $BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw -a 0x60000000 -j 0x60000000"
+	"-f $RESULT/bl32.bin.raw -a 0x50000000 -j 0x50000000"
+	"-f $RESULT/u-boot.bin.raw -a 0x43c00000 -j 0x43c00000"
+	)
 
-echo "*** USB Download $BL2 ***"
-sudo $DOWNLOADER -t nxp3220 -f $BL2 -a 0xffff8000 -j 0xffff8000
-sleep 8
-
-echo "*** USB Download $SSS ***"
-sudo $DOWNLOADER -t nxp3220 -f $SSS -a 0x60000000 -j 0x60000000
-sleep $WAITSEC
-
-echo "*** USB Download $BL32 ***"
-sudo $DOWNLOADER -t nxp3220 -f $BL32 -a 0x50000000 -j 0x50000000
-sleep $WAITSEC
-
-echo "*** USB Download $UBOOT ***"
-sudo $DOWNLOADER -t nxp3220 -f $UBOOT -a 0x43c00000 -j 0x43c00000
-sleep $WAITSEC
-
+for i in "${nxp3220[@]}"
+do
+	echo "DOWNLOAD: $i1"
+	sudo $DOWNLOADER -t nxp3220 $i
+	if [ $? -ne 0 ]; then
+		exit 1;
+	fi
+done
