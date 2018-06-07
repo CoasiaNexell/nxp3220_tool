1f4e8b362d443b3922d737acb90fc64576796d51
Deokjin Lee <truevirtue@nexell.co.kr>
2018-06-07 19:41:35 +0900

    tools: udown: modify the usb-downloader address
    This patch changed the download address to fit
    the memory size in artik310. SSS Firmware will
    be added later.
    
    Change-Id: Iff9046c7cb15e7ee3a251d61966cc60900870387
    Signed-off-by: Deokjin Lee <truevirtue@nexell.co.kr>

--- scripts/udown.sh
+++ scripts/udown.sh
@@ -6,10 +6,10 @@ DOWNLOADER=$BASEDIR/../linux-usbdownloader/linux-usbdownloader
 
 nxp3220=(
 	"-f $RESULT/nxp3220_bl1.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
-	"-f $RESULT/bl2-vtk.bin.raw -a 0xffff8000 -j 0xffff8000"
-	"-f $BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw -a 0x60000000 -j 0x60000000"
-	"-f $RESULT/bl32.bin.raw -a 0x50000000 -j 0x50000000"
-	"-f $RESULT/u-boot.bin.raw -a 0x43c00000 -j 0x43c00000"
+	"-f $RESULT/bl2-vtk.bin.raw -a 0xFFFF8000 -j 0xFFFF8000"
+#	"-f $BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw -a 0x60000000 -j 0x60000000"
+	"-f $RESULT/bl32.bin.raw -a 0x5E000000 -j 0x5E000000"
+	"-f $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
 	)
 
 for i in "${nxp3220[@]}"
