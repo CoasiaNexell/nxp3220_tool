af20046d0828a62fab060f6d988c4c2b11a5ead9
jhkim <jhkim@nexell.co.kr>
2018-06-08 20:05:21 +0900

    tools: udown: add usb download input file
    this patch add usb download input file
     #> udown.sh
     : download all boot images (bl1/bl1/bl32/u-boot.bin.raw)
    
     #> udown.sh [file]
     : download input file only
    
    Change-Id: I4b97c2668c45da0310c5a5694a31241574c62da0
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- scripts/udown.sh
+++ scripts/udown.sh
@@ -3,8 +3,9 @@
 BASEDIR=$(cd "$(dirname "$0")" && pwd)
 RESULT=$BASEDIR/../../result
 DOWNLOADER=$BASEDIR/../linux-usbdownloader/linux-usbdownloader
+TARGET=nxp3220
 
-nxp3220=(
+BLIMAGES=(
 	"-b $RESULT/nxp3220_bl1.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
 	"-b $RESULT/bl2-vtk.bin.raw -a 0xFFFF8000 -j 0xFFFF8000"
 #	"-b $BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw -a 0x60000000 -j 0x60000000"
@@ -12,12 +13,25 @@ nxp3220=(
 	"-b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
 	)
 
-for i in "${nxp3220[@]}"
-do
-	echo "DOWNLOAD: $i1"
-	sudo $DOWNLOADER -t nxp3220 $i
-	sleep 2	# wait for next connect
-	if [ $? -ne 0 ]; then
+# usb download input file
+if [ $# -eq 1 ]; then
+	echo "DOWNLOAD: $1"
+	if [ ! -f $1 ]; then
+		echo "No such file: $1 ... "
 		exit 1;
 	fi
+
+	sudo $DOWNLOADER -t $TARGET -f $1
+	exit $?
+fi
+
+# usb download BLIMAGES
+for i in "${BLIMAGES[@]}"
+do
+	echo "DOWNLOAD: $i"
+	sudo $DOWNLOADER -t $TARGET $i
+
+	[ $? -ne 0 ] && exit 1;
+
+	sleep 2	# wait for next connect
 done
