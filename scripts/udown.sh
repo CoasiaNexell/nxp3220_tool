85b9f0d0d88709b459f5e1cda884948e8b29e9cf
Deokjin Lee <truevirtue@nexell.co.kr>
2018-06-08 19:12:11 +0900

    tools: udown: modify the udown sciprt
    This patch has been modified for usb downloader
    as usage varies.
    
    Change-Id: Id8c5925054c30e2f12c6aedadbddce0b0d39e29f
    Signed-off-by: Deokjin Lee <truevirtue@nexell.co.kr>

--- scripts/udown.sh
+++ scripts/udown.sh
@@ -5,18 +5,18 @@ RESULT=$BASEDIR/../../result
 DOWNLOADER=$BASEDIR/../linux-usbdownloader/linux-usbdownloader
 
 nxp3220=(
-	"-f $RESULT/nxp3220_bl1.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
-	"-f $RESULT/bl2-vtk.bin.raw -a 0xFFFF8000 -j 0xFFFF8000"
-#	"-f $BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw -a 0x60000000 -j 0x60000000"
-	"-f $RESULT/bl32.bin.raw -a 0x5E000000 -j 0x5E000000"
-	"-f $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+	"-b $RESULT/nxp3220_bl1.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
+	"-b $RESULT/bl2-vtk.bin.raw -a 0xFFFF8000 -j 0xFFFF8000"
+#	"-b $BASEDIR/../../bl2/bl2-nxp3220/tools/sss.raw -a 0x60000000 -j 0x60000000"
+	"-b $RESULT/bl32.bin.raw -a 0x5E000000 -j 0x5E000000"
+	"-b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
 	)
 
 for i in "${nxp3220[@]}"
 do
 	echo "DOWNLOAD: $i1"
 	sudo $DOWNLOADER -t nxp3220 $i
-	sleep 1	# wait for next connect
+	sleep 2	# wait for next connect
 	if [ $? -ne 0 ]; then
 		exit 1;
 	fi
