f6fd92e65638af29a3564e31bc3824abacc5958f
Youngbok Park <ybpark@nexell.co.kr>
2018-11-08 13:05:18 +0900

    script: add nxp3220 evb board script
    This patch add nxp3220 evb board sctipts.
    
    Change-Id: I63f1242fd2060ba4f6090d35431640a5327f717d
    Signed-off-by: Youngbok Park <ybpark@nexell.co.kr>

---
+++ scripts/configs/udown.nxp3220-evb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "$0")" && pwd)/../.."
+RESULT="$BASEDIR/result"
+TARGET=nxp3220
+
+DN_IMAGES=(
+	"TARGET: $TARGET"
+	"BOARD : vtk"
+	"bl1   : -b $RESULT/bl1-nxp3220.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
+	"bl2   : -b $RESULT/bl2-vtk.bin.raw -a 0xFFFF9000 -j 0xFFFF9000"
+#	"sss   : -b $RESULT/sss.raw -a 0x23D18000 -j 0x23D18000"
+	"bl32  : -b $RESULT/bl32.bin.raw -a 0x5F000000 -j 0x5F000000"
+	"uboot : -b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+	"kernel: -f $RESULT/zImage"
+	"dtb   : -f $RESULT/nxp3220-evb.dtb"
+)
+
+DN_ENC_IMAGES=(
+	"TARGET: $TARGET"
+	"BOARD : vtk"
+	"bl1   : -b $RESULT/bl1-nxp3220.bin.enc.raw  -a 0xFFFF0000 -j 0xFFFF0000"
+	"bl2   : -b $RESULT/bl2-vtk.bin.raw -a 0xFFFF9000 -j 0xFFFF9000"
+#	"sss   : -b $RESULT/sss.raw -a 0x23D18000 -j 0x23D18000"
+	"bl32  : -b $RESULT/bl32.bin.enc.raw -a 0x5F000000 -j 0x5F000000"
+	"uboot : -b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+	"kernel: -f $RESULT/zImage"
+	"dtb   : -f $RESULT/nxp3220-evb.dtb"
+)
