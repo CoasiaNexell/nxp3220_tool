5ab46bad1ab177bf3d334f5e8ba7e23e97ead18f
jhkim <jhkim@nexell.co.kr>
2018-11-12 17:41:37 +0900

    scripts: add udown.bootloader.sh for usb download
    This patch add add udown.bootloader.sh for usb download
    
    Change-Id: If9d263e7f46d8030821e4a39621565ae76a259c4
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ scripts/configs/udown.bootloader.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "$0")" && pwd)/../.."
+RESULT="$BASEDIR/result"
+TARGET=nxp3220
+
+DN_IMAGES=(
+	"TARGET: $TARGET"
+	"bl1   : -b $RESULT/bl1-nxp3220.bin.raw -a 0xFFFF0000 -j 0xFFFF0000"
+	"bl2   : -b $RESULT/bl2.bin.raw -a 0xFFFF9000 -j 0xFFFF9000"
+#	"sss   : -b $RESULT/sss.raw -a 0x23D18000 -j 0x23D18000"
+	"bl32  : -b $RESULT/bl32.bin.raw -a 0x5F000000 -j 0x5F000000"
+	"uboot : -b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+)
+
+DN_ENC_IMAGES=(
+	"TARGET: $TARGET"
+	"bl1   : -b $RESULT/bl1-nxp3220.bin.enc.raw -a 0xFFFF0000 -j 0xFFFF0000"
+	"bl2   : -b $RESULT/bl2.bin.raw -a 0xFFFF9000 -j 0xFFFF9000"
+#	"sss   : -b $RESULT/sss.raw -a 0x23D18000 -j 0x23D18000"
+	"bl32  : -b $RESULT/bl32.bin.enc.raw -a 0x5F000000 -j 0x5F000000"
+	"uboot : -b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+)
