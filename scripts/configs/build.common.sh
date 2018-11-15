42839b65dd0599222b73dbd72dbef80af618478f
jhkim <jhkim@nexell.co.kr>
2018-11-15 19:24:24 +0900

    tools: configs: modify build macros for each board build
    This patch modify build macros for each board build
    
    Change-Id: I4acdd91b1eac3cdb8a4a133f7d73643b065776ae
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- scripts/configs/build.common.sh
+++ scripts/configs/build.common.sh
@@ -22,23 +22,22 @@ BINGEN_EXE="$BIN_DIR/bingen"
 
 # BL2 BUILD
 BL2_DIR=$BASEDIR/firmwares/bl2-nxp3220
-BL2_MAKEOPT="CHIPNAME=${TARGET_CHIP_ALIAS} BOARD=${TARGET_BOARD} PMIC=nxe1500"
-BL2_NSIH="$BL2_DIR/reference-nsih/nsih_${TARGET_BOARD}_ddr3_800Mhz.txt"
+BL2_MAKEOPT="CHIPNAME=${TARGET_BL2_CHIP} BOARD=${TARGET_BL2_BOARD} PMIC=${TARGET_BL2_PMIC}"
+BL2_NSIH="$BL2_DIR/reference-nsih/${TARGET_BL2_NSIH}.txt"
 
-BL2_BINGEN="$BINGEN_EXE -n $BL2_NSIH -i $RESULT/bl2-${TARGET_BOARD}.bin
+BL2_BINGEN="$BINGEN_EXE -n $BL2_NSIH -i $RESULT/bl2-${TARGET_BL2_BOARD}.bin
 		-b $BOOT_KEY -u $USER_KEY -k bl2 -l 0xFFFF9000 -s 0xFFFF9000 -t"
 BL2_COMMAND="$BL2_BINGEN; \
-		cp $RESULT/bl2-${TARGET_BOARD}.bin.raw $RESULT/bl2.bin.raw"
+		cp $RESULT/bl2-${TARGET_BL2_BOARD}.bin.raw $RESULT/bl2.bin.raw"
 
 # BL32 BUILD
 BL32_DIR=$BASEDIR/firmwares/bl32-nxp3220
 BL32_NSIH="$BL32_DIR/reference-nsih/nsih_general.txt"
+
 BL32_BINGEN="$BINGEN_EXE -n $BL32_NSIH -i $RESULT/bl32.bin
 		-b $BOOT_KEY -u $USER_KEY -k bl32 -l 0x5F000000 -s 0x5F000000 -t"
-
 BL32_BINGEN_ENC="$BINGEN_EXE -n $BL32_NSIH -i $RESULT/bl32.bin.enc
 		-b $BOOT_KEY -u $USER_KEY -k bl32 -l 0x5F000000 -s 0x5F000000 -t"
-
 UBOOT_BINGEN="$BINGEN_EXE -n $UBOOT_NSIH -i $RESULT/u-boot.bin
 		-b $BOOT_KEY -u $USER_KEY -k bl33 -l 0x43C00000 -s 0x43C00000 -t"
 
@@ -47,32 +46,30 @@ AESKEY=$(<$FILES_DIR/aeskey.txt)
 AESVECTOR=$(<$FILES_DIR/aesvector.txt)
 
 BL32_AESCBC_ENC="$AESCBC_EXE -n $RESULT/bl32.bin -k $AESKEY -v $AESVECTOR -m enc -b 128"
-
 BL32_COMMAND="$BL32_AESCBC_ENC; $BL32_BINGEN_ENC; $BL32_BINGEN"
 
-
 # Images BUILD
-MAKE_EXT4FS_EXE="$BASEDIR/tools/bin/make_ext4fs"
+EXT4FS_EXE="$BASEDIR/tools/bin/make_ext4fs"
+
 MAKE_BOOTIMG="mkdir -p $RESULT/boot; \
 		cp -a $RESULT/zImage $RESULT/boot; \
-		cp -a $RESULT/${TARGET_CHIP}-${TARGET_BOARD}.dtb $RESULT/boot; \
-		$MAKE_EXT4FS_EXE -b 4096 -L boot -l 33554432 $RESULT/boot.img $RESULT/boot/"
-
-MAKE_ROOTIMG="$MAKE_EXT4FS_EXE -b 4096 -L rootfs -l 1073741824 $RESULT/rootfs.img $RESULT/rootfs"
+		cp -a $RESULT/${TARGET_KERNEL_DTB}.dtb $RESULT/boot; \
+		$EXT4FS_EXE -b 4096 -L boot -l 33554432 $RESULT/boot.img $RESULT/boot/"
+MAKE_ROOTIMG="$EXT4FS_EXE -b 4096 -L rootfs -l 1073741824 $RESULT/rootfs.img $RESULT/rootfs"
 
 # Build Targets
 BUILD_IMAGES=(
-	"MACHINE= ${TARGET_CHIP}",
+	"MACHINE= nxp3220",
 	"ARCH  	= arm",
 	"TOOL	= $LINUX_TOOLCHAIN",
 	"RESULT = $RESULT",
 	"bl1   	=
-		OUTPUT	: $BASEDIR/firmwares/binary/bl1-${TARGET_CHIP}.bin.raw",
+		OUTPUT	: $BASEDIR/firmwares/binary/bl1-${TARGET_BL1_NAME}.bin.raw",
 	"bl2   	=
 		PATH  	: $BL2_DIR,
 		TOOL  	: $BL_TOOLCHAIN,
 		OPTION	: $BL2_MAKEOPT,
-		OUTPUT	: out/bl2-${TARGET_BOARD}.bin*,
+		OUTPUT	: out/bl2-${TARGET_BL2_BOARD}.bin*,
 		POSTCMD : $BL2_COMMAND,
 		JOBS  	: 1", # must be 1
 	"bl32   	=
@@ -83,24 +80,24 @@ BUILD_IMAGES=(
 		JOBS  	: 1", # must be 1
 	"uboot 	=
 		PATH  	: $UBOOT_DIR,
-		CONFIG	: ${TARGET_CHIP_ALIAS}_${TARGET_BOARD}_defconfig,
+		CONFIG	: ${TARGET_UBOOT_DEFCONFIG},
 		OUTPUT	: u-boot.bin,
 		POSTCMD	: $UBOOT_BINGEN"
 	"br2   	=
 		PATH  	: $BR2_DIR,
-		CONFIG	: ${TARGET_CHIP}_sysv_defconfig,
+		CONFIG	: ${TARGET_BR2_DEFCONFIG},
 		OUTPUT	: output/target,
 		COPY  	: rootfs",
 	"kernel	=
 		PATH  	: $KERNEL_DIR,
-		CONFIG	: ${TARGET_CHIP_ALIAS}_${TARGET_BOARD}_defconfig,
+		CONFIG	: ${TARGET_KERNEL_DEFCONFIG},
 		IMAGE 	: zImage,
 		OUTPUT	: arch/arm/boot/zImage,
 		POSTCMD : $MAKE_BOOTIMG",
 	"dtb   	=
 		PATH  	: $KERNEL_DIR,
-		IMAGE 	: ${TARGET_CHIP}-${TARGET_BOARD}.dtb,
-		OUTPUT	: arch/arm/boot/dts/${TARGET_CHIP}-${TARGET_BOARD}.dtb,
+		IMAGE 	: ${TARGET_KERNEL_DTB}.dtb,
+		OUTPUT	: arch/arm/boot/dts/${TARGET_KERNEL_DTB}.dtb,
 		POSTCMD : $MAKE_BOOTIMG",
 	"bootimg =
 		POSTCMD : $MAKE_BOOTIMG",
--- scripts/configs/build.sip-s31nx-vtk.sh
+++ scripts/configs/build.sip-s31nx-vtk.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-TARGET_CHIP=sip-s31nx
-TARGET_CHIP_ALIAS=sip_s31nx
-TARGET_BOARD=vtk
+TARGET_BL1_NAME=nxp3220
+
+TARGET_BL2_CHIP=sip_s31nx
+TARGET_BL2_BOARD=vtk
+TARGET_BL2_PMIC=sm5011
+TARGET_BL2_NSIH=nsih_vtk_ddr3_800Mhz
+
+TARGET_KERNEL_DEFCONFIG=sip_s31nx_vtk_defconfig
+TARGET_KERNEL_DTB=sip-s31nx-vtk
+
+TARGET_UBOOT_DEFCONFIG=sip_s31nx_vtk_defconfig
+
+TARGET_BR2_DEFCONFIG=nxp3220_sysv_defconfig
 
 CONFIGDIR="$(cd "$(dirname "$0")" && pwd)"/configs
 
