876459b6c2691b8dcddb7962bb28720ffde88d4b
DeokJin Lee <truevirtue@nexell.co.kr>
2019-01-11 11:40:44 +0900

    tools: configs: modify the udown scripts
    This patch removes the load, launch address from
    the udown scripts.
    
    Signed-off-by: DeokJin Lee <truevirtue@nexell.co.kr>
    Change-Id: Ib6f57bb533df2c483fbec877b9439c157acc700f
    Reviewed-on: https://review.gerrithub.io/c/439940
    Reviewed-by: jung hyun kim <jhkim@nexell.co.kr>
    Tested-by: jung hyun kim <jhkim@nexell.co.kr>

--- scripts/configs/udown.nxp3220-evb.sh
+++ scripts/configs/udown.nxp3220-evb.sh
@@ -6,12 +6,12 @@ TARGET=nxp3220
 
 DN_IMAGES=(
 	"TARGET: $TARGET"
-	"BOARD : vtk"
-	"bl1   : -b $RESULT/bl1-nxp3220.bin.raw  -a 0xFFFF0000 -j 0xFFFF0000"
-	"bl2   : -b $RESULT/bl2-vtk.bin.raw -a 0xFFFF9000 -j 0xFFFF9000"
-#	"sss   : -b $RESULT/sss.raw -a 0x23D18000 -j 0x23D18000"
-	"bl32  : -b $RESULT/bl32.bin.raw -a 0x5F000000 -j 0x5F000000"
-	"uboot : -b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+	"BOARD : evb"
+	"bl1   : -b $RESULT/bl1-nxp3220.bin.raw"
+	"bl2   : -b $RESULT/bl2-evb.bin.raw"
+#	"sss   : -b $RESULT/sss.raw"
+	"bl32  : -b $RESULT/bl32.bin.raw"
+	"uboot : -b $RESULT/u-boot.bin.raw"
 	"kernel: -f $RESULT/zImage"
 	"dtb   : -f $RESULT/nxp3220-evb.dtb"
 )
@@ -19,11 +19,11 @@ DN_IMAGES=(
 DN_ENC_IMAGES=(
 	"TARGET: $TARGET"
 	"BOARD : vtk"
-	"bl1   : -b $RESULT/bl1-nxp3220.bin.enc.raw  -a 0xFFFF0000 -j 0xFFFF0000"
-	"bl2   : -b $RESULT/bl2-vtk.bin.raw -a 0xFFFF9000 -j 0xFFFF9000"
-#	"sss   : -b $RESULT/sss.raw -a 0x23D18000 -j 0x23D18000"
-	"bl32  : -b $RESULT/bl32.bin.enc.raw -a 0x5F000000 -j 0x5F000000"
-	"uboot : -b $RESULT/u-boot.bin.raw -a 0x43C00000 -j 0x43C00000"
+	"bl1   : -b $RESULT/bl1-nxp3220.bin.enc.raw"
+	"bl2   : -b $RESULT/bl2-evb.bin.raw"
+#	"sss   : -b $RESULT/sss.raw"
+	"bl32  : -b $RESULT/bl32.bin.enc.raw"
+	"uboot : -b $RESULT/u-boot.bin.raw"
 	"kernel: -f $RESULT/zImage"
 	"dtb   : -f $RESULT/nxp3220-evb.dtb"
 )
