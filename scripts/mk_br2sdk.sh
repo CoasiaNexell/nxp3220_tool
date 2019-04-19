ba39f7daa82fea33b64fa7c9426fee451e4ca668
jhkim <jhkim@nexell.co.kr>
2019-04-19 18:21:00 +0900

    tools: mk_br2sdk.sh: fix invalid help message
    This patch fix invalid help message
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I0382657d0c466a0212739fa954509e26a6f40c17
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/nxp3220_tool/+/451518

--- scripts/mk_br2sdk.sh
+++ scripts/mk_br2sdk.sh
@@ -17,7 +17,7 @@ function help() {
 	echo  -e "\033[0;33m  \033[0m"
 	echo  -e "\033[0;33m Setup SDK: \033[0m"
 	echo  -e "\033[0;33m  $> tar zxf $RESULTDIR/${BUILDROOT_SDK}.tar.gz -C <SDK PATH>\033[0m"
-	echo  -e "\033[0;33m  $> <SDK PATH>/$BUILDROOT_SDK$BUILDROOT_SDK$BUILDROOT_SDK///relocate-sdk.sh\033[0m"
+	echo  -e "\033[0;33m  $> <SDK PATH>/$BUILDROOT_SDK/relocate-sdk.sh\033[0m"
 	echo  -e "\033[0;33m  \033[0m"
 	echo  -e "\033[0;33m Setup Environments: \033[0m"
 	echo  -e "\033[0;33m  $> source ${BASEDIR}/tools/scripts/env_setup_br2_sdk.sh <SDK PATH>\033[0m"
