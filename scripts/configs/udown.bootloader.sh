898ac7db1f9aa914dc8344eeecea9c2034c2e1ae
jhkim <jhkim@nexell.co.kr>
2019-05-03 09:44:52 +0900

    tools: configs: change download secure os image when '-p' option
    This patch change download secure os image when '-p' option
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ie68b4cbdf39d5d7156a5eaa2d2ad02ba5a4799e2
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/nxp3220_tool/+/452937

--- scripts/configs/udown.bootloader.sh
+++ scripts/configs/udown.bootloader.sh
@@ -17,6 +17,6 @@ DN_ENC_IMAGES=(
 	"bl1   : -b $RESULTDIR/bl1-nxp3220.bin.enc.raw"
 	"bl2   : -b $RESULTDIR/bl2.bin.raw"
 #	"sss   : -b $RESULTDIR/sss.raw"
-	"bl32  : -b $RESULTDIR/bl32.bin.enc.raw"
+	"bl32  : -b $RESULTDIR/bl32.bin.raw"
 	"uboot : -b $RESULTDIR/u-boot.bin.raw"
 )
