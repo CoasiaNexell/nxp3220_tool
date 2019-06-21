f2c09534d203cb0a006c1598fcbb306ab0dd2483
jhkim <jhkim@nexell.co.kr>
2019-06-21 10:52:50 +0900

    script: init add nxp3220 daudio2 board script
    This patch init add nxp3220 daudio2 board sctipts.
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I972179f1ddbcf25822e28e7d21fc643abd9c6304
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/nxp3220_tool/+/458911

---
+++ scripts/configs/build.nxp3220-daudio2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "$0")" && pwd)/../.."
+RESULT="$BASEDIR/result"
+
+TARGET_BL1_NAME=nxp3220
+
+TARGET_BL2_CHIP=nxp3220
+TARGET_BL2_BOARD=evb
+TARGET_BL2_PMIC=nxe1500
+TARGET_BL2_NSIH=nsih_evb_ddr3_800Mhz
+
+TARGET_BL32_LOADADDRESS=0x5F000000
+TARGET_BL32_LAUNCHADDRESS=0x5F000000
+
+TARGET_KERNEL_DEFCONFIG=nxp3220_daudio2_defconfig
+TARGET_KERNEL_DTB=nxp3220-daudio2-rev00
+TARGET_KERNEL_IMAGE=Image
+
+TARGET_UBOOT_DEFCONFIG=nxp3220_daudio2_defconfig
+
+TARGET_BR2_DEFCONFIG=nxp3220_sysv_defconfig
+
+BOOT_IMAGE_SIZE=32M
+ROOT_IMAGE_SIZE=1G
+
+CONFIGDIR="$(cd "$(dirname "$0")" && pwd)"/configs
+
+source $CONFIGDIR/build.common.sh
