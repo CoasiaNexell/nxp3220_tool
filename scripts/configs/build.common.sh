a5372037895ba746507add284daccc639a8fadf4
jhkim <jhkim@nexell.co.kr>
2020-04-08 13:01:02 +0900

    tools: build_bsp: Fix build macros
    This patch fix build macros
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I8f0f9ddb8a018d5829d9628a75be49152da16686
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/nxp3220_tool/+/489059

--- scripts/configs/build.common.sh
+++ scripts/configs/build.common.sh
@@ -9,19 +9,19 @@ function msg() {
 }
 
 # Add to build source at target script:
-# export BASEDIR=`realpath "$(cd "$(dirname "$0")" && pwd)/../.."`
-# TARGET_BL1_DIR=${BASEDIR}/firmwares/bl1-nxp3220
+# export BSP_BASEDIR=`realpath "$(cd "$(dirname "$0")" && pwd)/../.."`
+# TARGET_BL1_DIR=${BSP_BASEDIR}/firmwares/bl1-nxp3220
 
 SECURE_BL1_IVECTOR=73FC7B44B996F9990261A01C9CB93C8F
 SECURE_BL32_IVECTOR=73FC7B44B996F9990261A01C9CB93C8F
 
 function post_build_bl1 () {
-	local bl1_binary=${BL1_DIR}/${BL1_BIN}
-	local outdir=${BL1_DIR}
+	local bl1_binary=${BSP_BL1_DIR}/${BL1_BIN}
+	local outdir=${BSP_BL1_DIR}
 
 	if [[ ! -z $TARGET_BL1_DIR ]]; then
-		bl1_binary=${BL1_DIR}/out/${BL1_BIN}
-		outdir=${BL1_DIR}/out
+		bl1_binary=${BSP_BL1_DIR}/out/${BL1_BIN}
+		outdir=${BSP_BL1_DIR}/out
 	fi
 
 	# Copy encrypt keys
@@ -48,166 +48,166 @@ function post_build_bl1 () {
         ${TOOL_BINGEN} -k bl1 -n ${BL1_NSIH} -i ${bl1_binary} \
 		-b ${SECURE_BOOTKEY} -u ${SECURE_USERKEY} -l ${BL1_LOADADDR} -s ${BL1_LOADADDR} -t;
 
-	cp ${SECURE_BL1_ENCKEY} ${RESULT}
-	cp ${SECURE_BOOTKEY}.pub.hash.txt ${RESULT}
+	cp ${SECURE_BL1_ENCKEY} ${BSP_RESULT}
+	cp ${SECURE_BOOTKEY}.pub.hash.txt ${BSP_RESULT}
 
-	cp ${bl1_binary}.raw ${RESULT}
-	cp ${bl1_binary}.enc.raw ${RESULT}
+	cp ${bl1_binary}.raw ${BSP_RESULT}
+	cp ${bl1_binary}.enc.raw ${BSP_RESULT}
 }
 
 function post_build_bl2 () {
         # Binary + NSIH : $BIN.raw
 	msg "BINGEN : ${BL2_BIN} -> ${BL2_BIN}.raw"
-        ${TOOL_BINGEN} -k bl2 -n ${BL2_NSIH} -i ${BL2_DIR}/out/${BL2_BIN} \
+        ${TOOL_BINGEN} -k bl2 -n ${BL2_NSIH} -i ${BSP_BL2_DIR}/out/${BL2_BIN} \
 		-b ${SECURE_BOOTKEY} -u ${SECURE_USERKEY} -l ${BL2_LOADADDR} -s ${BL2_LOADADDR} -t;
 
-        cp ${BL2_DIR}/out/${BL2_BIN}.raw ${RESULT}/bl2.bin.raw;
+        cp ${BSP_BL2_DIR}/out/${BL2_BIN}.raw ${BSP_RESULT}/bl2.bin.raw;
 }
 
 function post_build_bl32 () {
 	# Copy encrype keys
-	cp ${SECURE_BL32_ENCKEY}  ${BL32_DIR}/out/$(basename $SECURE_BL32_ENCKEY)
-	cp ${SECURE_BOOTKEY} ${BL32_DIR}/out/$(basename $SECURE_BOOTKEY)
-	cp ${SECURE_USERKEY} ${BL32_DIR}/out/$(basename $SECURE_USERKEY)
+	cp ${SECURE_BL32_ENCKEY}  ${BSP_BL32_DIR}/out/$(basename $SECURE_BL32_ENCKEY)
+	cp ${SECURE_BOOTKEY} ${BSP_BL32_DIR}/out/$(basename $SECURE_BOOTKEY)
+	cp ${SECURE_USERKEY} ${BSP_BL32_DIR}/out/$(basename $SECURE_USERKEY)
 
-	SECURE_BL32_ENCKEY=${BL32_DIR}/out/$(basename $SECURE_BL32_ENCKEY)
-	SECURE_BOOTKEY=${BL32_DIR}/out/$(basename $SECURE_BOOTKEY)
-	SECURE_USERKEY=${BL32_DIR}/out/$(basename $SECURE_USERKEY)
+	SECURE_BL32_ENCKEY=${BSP_BL32_DIR}/out/$(basename $SECURE_BL32_ENCKEY)
+	SECURE_BOOTKEY=${BSP_BL32_DIR}/out/$(basename $SECURE_BOOTKEY)
+	SECURE_USERKEY=${BSP_BL32_DIR}/out/$(basename $SECURE_USERKEY)
 
         # Encrypt binary : $BIN.enc
 	msg "ENCRYPT: ${BL32_BIN} -> ${BL32_BIN}.enc"
 	${TOOL_BINENC} enc -e -nosalt -aes-128-cbc \
-		-in ${BL32_DIR}/out/${BL32_BIN} -out ${BL32_DIR}/out/${BL32_BIN}.enc \
+		-in ${BSP_BL32_DIR}/out/${BL32_BIN} -out ${BSP_BL32_DIR}/out/${BL32_BIN}.enc \
 		-K $(cat ${SECURE_BL32_ENCKEY}) -iv ${SECURE_BL32_IVECTOR};
 
         # (Encrypted binary) + NSIH : $BIN.enc.raw
 	msg "BINGEN : ${BL32_BIN}.enc -> ${BL32_BIN}.enc.raw"
-        ${TOOL_BINGEN} -k bl32 -n ${BL32_NSIH} -i ${BL32_DIR}/out/${BL32_BIN}.enc \
+        ${TOOL_BINGEN} -k bl32 -n ${BL32_NSIH} -i ${BSP_BL32_DIR}/out/${BL32_BIN}.enc \
 		-b ${SECURE_BOOTKEY} -u ${SECURE_USERKEY} \
 		-l ${BL32_LOADADDR} -s ${BL32_LOADADDR} -t -e;
 
         # Binary + NSIH : $BIN.raw
 	msg "BINGEN : ${BL32_BIN} -> ${BL32_BIN}.raw"
-        ${TOOL_BINGEN} -k bl32 -n ${BL32_NSIH} -i ${BL32_DIR}/out/${BL32_BIN} \
+        ${TOOL_BINGEN} -k bl32 -n ${BL32_NSIH} -i ${BSP_BL32_DIR}/out/${BL32_BIN} \
 		-b ${SECURE_BOOTKEY} -u ${SECURE_USERKEY} \
 		-l ${BL32_LOADADDR} -s ${BL32_LOADADDR} -t;
 
-	cp ${SECURE_BL32_ENCKEY} ${RESULT}
+	cp ${SECURE_BL32_ENCKEY} ${BSP_RESULT}
 
-	cp ${BL32_DIR}/out/${BL32_BIN}.raw ${RESULT}
-	cp ${BL32_DIR}/out/${BL32_BIN}.enc.raw ${RESULT}
+	cp ${BSP_BL32_DIR}/out/${BL32_BIN}.raw ${BSP_RESULT}
+	cp ${BSP_BL32_DIR}/out/${BL32_BIN}.enc.raw ${BSP_RESULT}
 }
 
 function pre_build_uboot () {
-	file=${UBOOT_DIR}/.uboot_defconfig
+	file=${BSP_UBOOT_DIR}/.uboot_defconfig
 	[ -e ${file} ] && [[ $(cat ${file}) == "${UBOOT_DEFCONFIG}+bsp" ]] && return;
 	rm -f ${file}; echo "${UBOOT_DEFCONFIG}+bsp" >> ${file};
-	make -C ${UBOOT_DIR} distclean
+	make -C ${BSP_UBOOT_DIR} distclean
 }
 
 function post_build_uboot () {
 	msg "BINGEN : ${UBOOT_BIN} -> ${UBOOT_BIN}.raw"
-        ${TOOL_BINGEN} -k bl33 -n ${UBOOT_NSIH} -i ${UBOOT_DIR}/${UBOOT_BIN} \
+        ${TOOL_BINGEN} -k bl33 -n ${UBOOT_NSIH} -i ${BSP_UBOOT_DIR}/${UBOOT_BIN} \
 		-b ${SECURE_BOOTKEY} -u ${SECURE_USERKEY} \
 		-l ${UBOOT_LOADADDR} -s ${UBOOT_LOADADDR} -t;
 
-	cp ${UBOOT_DIR}/${UBOOT_BIN}.raw ${RESULT}
+	cp ${BSP_UBOOT_DIR}/${UBOOT_BIN}.raw ${BSP_RESULT}
 
 	# create param.bin
-	${TOOL_MKPARAM} ${UBOOT_DIR} ${LINUX_TOOLCHAIN} ${RESULT}
+	${TOOL_MKPARAM} ${BSP_UBOOT_DIR} ${BSP_TOOLCHAIN_LINUX} ${BSP_RESULT}
 }
 
 function pre_build_kernel () {
-	file=${KERNEL_DIR}/.kernel_defconfig
+	file=${BSP_KERNEL_DIR}/.kernel_defconfig
 	[ -e ${file} ] && [[ $(cat ${file}) == "${KERNEL_DEFCONFIG}+bsp" ]] && return;
 	rm -f ${file}; echo "${KERNEL_DEFCONFIG}+bsp" >> ${file};
-	make -C ${KERNEL_DIR} distclean
+	make -C ${BSP_KERNEL_DIR} distclean
 }
 
 function post_copy_tools () {
-	for file in "${TOOL_FILES[@]}"; do
+	for file in "${BSP_TOOL_FILES[@]}"; do
 		[[ -d $file ]] && continue;
-		cp -a $file ${RESULT}
+		cp -a $file ${BSP_RESULT}
 	done
 }
 
 function post_data_image () {
 	[[ ! ${IMAGE_DATA_SIZE} ]] || [[ ${IMAGE_TYPE} == "ubi" ]] && return;
-	[[ ! -d $RESULT/userdata ]] && mkdir -p $RESULT/userdata;
+	[[ ! -d $BSP_RESULT/userdata ]] && mkdir -p $BSP_RESULT/userdata;
 
-	${TOOL_MKEXT4} -b 4096 -s -L userdata -l ${IMAGE_DATA_SIZE} $RESULT/userdata.img $RESULT/userdata
+	${TOOL_MKEXT4} -b 4096 -s -L userdata -l ${IMAGE_DATA_SIZE} $BSP_RESULT/userdata.img $BSP_RESULT/userdata
 }
 
 function post_ret_link () {
 
-	if [[ -e ${BASEDIR}/out/result  ]] ||
-	   [[ -h ${BASEDIR}/out/result ]]; then
-		rm -f ${BASEDIR}/out/result
+	if [[ -e ${BSP_BASEDIR}/out/result  ]] ||
+	   [[ -h ${BSP_BASEDIR}/out/result ]]; then
+		rm -f ${BSP_BASEDIR}/out/result
 	fi
 
-	ln -s ${RESULT} ${BASEDIR}/out/result
+	ln -s ${BSP_RESULT} ${BSP_BASEDIR}/out/result
 }
 
 function pre_boot_image () {
-	mkdir -p ${RESULT}/boot;
-	cp -a ${RESULT}/${KERNEL_BIN} ${RESULT}/boot;
-	cp -a ${RESULT}/${DTB_BIN} ${RESULT}/boot;
+	mkdir -p ${BSP_RESULT}/boot;
+	cp -a ${BSP_RESULT}/${KERNEL_BIN} ${BSP_RESULT}/boot;
+	cp -a ${BSP_RESULT}/${DTB_BIN} ${BSP_RESULT}/boot;
 	if [ -f ${UBOOT_LOGO_BMP} ]; then
-		cp -a ${UBOOT_LOGO_BMP} ${RESULT}/boot;
+		cp -a ${UBOOT_LOGO_BMP} ${BSP_RESULT}/boot;
 	fi
 }
 
 if [[ ${IMAGE_TYPE} == "ubi" ]]; then
-	BOOT_POSTCMD="${TOOL_MKUBIFS} -r ${RESULT}/boot -v boot -i 0 -l ${IMAGE_BOOT_SIZE}
+	BOOT_POSTCMD="${TOOL_MKUBIFS} -r ${BSP_RESULT}/boot -v boot -i 0 -l ${IMAGE_BOOT_SIZE}
 			-p ${FLASH_PAGE_SIZE} -b ${FLASH_BLOCK_SIZE} -c ${FLASH_DEVICE_SIZE}"
-	ROOT_POSTCMD="${TOOL_MKUBIFS} -r ${RESULT}/rootfs -v rootfs -i 1 -l ${IMAGE_ROOT_SIZE}
+	ROOT_POSTCMD="${TOOL_MKUBIFS} -r ${BSP_RESULT}/rootfs -v rootfs -i 1 -l ${IMAGE_ROOT_SIZE}
 			-p ${FLASH_PAGE_SIZE} -b ${FLASH_BLOCK_SIZE} -c ${FLASH_DEVICE_SIZE}"
 else
-	BOOT_POSTCMD="${TOOL_MKEXT4} -L boot -s -b 4k -l ${IMAGE_BOOT_SIZE} $RESULT/boot.img $RESULT/boot/"
-	ROOT_POSTCMD="${TOOL_MKEXT4} -L rootfs -s -b 4k -l ${IMAGE_ROOT_SIZE} $RESULT/rootfs.img $RESULT/rootfs"
+	BOOT_POSTCMD="${TOOL_MKEXT4} -L boot -s -b 4k -l ${IMAGE_BOOT_SIZE} $BSP_RESULT/boot.img $BSP_RESULT/boot/"
+	ROOT_POSTCMD="${TOOL_MKEXT4} -L rootfs -s -b 4k -l ${IMAGE_ROOT_SIZE} $BSP_RESULT/rootfs.img $BSP_RESULT/rootfs"
 fi
 
 # Build Targets
 BUILD_IMAGES=(
 	"MACHINE= nxp3220",
 	"ARCH  	= arm",
-	"TOOL	= ${LINUX_TOOLCHAIN}",
-	"RESULT = ${RESULT}",
+	"TOOL	= ${BSP_TOOLCHAIN_LINUX}",
+	"RESULT = ${BSP_RESULT}",
 	"bl1   	=
-		PATH  	: ${BL1_DIR},
-		TOOL  	: ${BL_TOOLCHAIN},
+		PATH  	: ${BSP_BL1_DIR},
+		TOOL  	: ${BSP_TOOLCHAIN_BL},
 		POSTCMD : post_build_bl1,
 		JOBS  	: 1", # must be 1
 	"bl2   	=
-		PATH  	: ${BL2_DIR},
-		TOOL  	: ${BL_TOOLCHAIN},
+		PATH  	: ${BSP_BL2_DIR},
+		TOOL  	: ${BSP_TOOLCHAIN_BL},
 		OPTION	: CHIPNAME=${BL2_CHIP} BOARD=${BL2_BOARD} PMIC=${BL2_PMIC},
 		POSTCMD : post_build_bl2,
 		JOBS  	: 1", # must be 1
 	"bl32  =
-		PATH  	: ${BL32_DIR},
-		TOOL  	: ${BL_TOOLCHAIN},
+		PATH  	: ${BSP_BL32_DIR},
+		TOOL  	: ${BSP_TOOLCHAIN_BL},
 		POSTCMD	: post_build_bl32,
 		JOBS  	: 1", # must be 1
 	"uboot 	=
-		PATH  	: ${UBOOT_DIR},
+		PATH  	: ${BSP_UBOOT_DIR},
 		CONFIG	: ${UBOOT_DEFCONFIG},
 		OUTPUT	: u-boot.bin,
 		PRECMD	: pre_build_uboot,
 		POSTCMD	: post_build_uboot"
 	"br2   	=
-		PATH  	: ${BR2_DIR},
+		PATH  	: ${BSP_BR2_DIR},
 		CONFIG	: ${BR2_DEFCONFIG},
 		OUTPUT	: output/target,
 		COPY  	: rootfs",
 	"kernel	=
-		PATH  	: ${KERNEL_DIR},
+		PATH  	: ${BSP_KERNEL_DIR},
 		CONFIG	: ${KERNEL_DEFCONFIG},
 		IMAGE 	: ${KERNEL_BIN},
 		OUTPUT	: arch/arm/boot/${KERNEL_BIN},
 		PRECMD	: pre_build_kernel",
 	"dtb   	=
-		PATH  	: ${KERNEL_DIR},
+		PATH  	: ${BSP_KERNEL_DIR},
 		IMAGE 	: ${DTB_BIN},
 		OUTPUT	: arch/arm/boot/dts/${DTB_BIN}",
 	"bootimg =
--- scripts/configs/env_common.sh
+++ scripts/configs/env_common.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
-export BASEDIR=`realpath "$(cd "$(dirname "$0")" && pwd)/../.."`
-export RESULT="${BASEDIR}/out/result"
+export BSP_BASEDIR=`realpath "$(cd "$(dirname "$0")" && pwd)/../.."`
+export BSP_RESULT="${BSP_BASEDIR}/out/result"
 
 if [[ ! -z $TARGET_RESULT ]]; then
-	export RESULT="${BASEDIR}/out/${TARGET_RESULT}"
+	export BSP_RESULT="${BSP_BASEDIR}/out/${TARGET_RESULT}"
 fi
 
-if [[ -z $BL_TOOLCHAIN ]]; then
-	export BL_TOOLCHAIN="${BASEDIR}/tools/crosstools/gcc-arm-none-eabi-6-2017-q2-update/bin/arm-none-eabi-"
+if [[ -z $BSP_TOOLCHAIN_BL ]]; then
+	export BSP_TOOLCHAIN_BL="${BSP_BASEDIR}/tools/crosstools/gcc-arm-none-eabi-6-2017-q2-update/bin/arm-none-eabi-"
 fi
 
-if [[ -z $LINUX_TOOLCHAIN ]]; then
-	export LINUX_TOOLCHAIN="${BASEDIR}/tools/crosstools/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
+if [[ -z $BSP_TOOLCHAIN_LINUX ]]; then
+	export BSP_TOOLCHAIN_LINUX="${BSP_BASEDIR}/tools/crosstools/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
 fi
 
 if [[ -z $TARGET_BL1_DIR ]]; then
-	export BL1_DIR=${BASEDIR}/firmwares/binary
+	export BSP_BL1_DIR=${BSP_BASEDIR}/firmwares/binary
 else
-	export BL1_DIR=${TARGET_BL1_DIR}
+	export BSP_BL1_DIR=${TARGET_BL1_DIR}
 fi
 
-export BL2_DIR=${BASEDIR}/firmwares/bl2-nxp3220
-export BL32_DIR=${BASEDIR}/firmwares/bl32-nxp3220
-export UBOOT_DIR=${BASEDIR}/u-boot-2018.5
-export KERNEL_DIR=${BASEDIR}/kernel-4.14
-export BR2_DIR=${BASEDIR}/buildroot
+export BSP_BL2_DIR=${BSP_BASEDIR}/firmwares/bl2-nxp3220
+export BSP_BL32_DIR=${BSP_BASEDIR}/firmwares/bl32-nxp3220
+export BSP_UBOOT_DIR=${BSP_BASEDIR}/u-boot-2018.5
+export BSP_KERNEL_DIR=${BSP_BASEDIR}/kernel-4.14
+export BSP_BR2_DIR=${BSP_BASEDIR}/buildroot
 
-export TOOL_BINGEN="${BASEDIR}/tools/bin/bingen"
+export TOOL_BINGEN="${BSP_BASEDIR}/tools/bin/bingen"
 export TOOL_BINENC="openssl"
-export TOOL_BINECC="${BASEDIR}/tools/bin/nandbingen"
-export TOOL_MKPARAM="${BASEDIR}/tools/scripts/mk_bootparam.sh"
-export TOOL_MKUBIFS="${BASEDIR}/tools/scripts/mk_ubifs.sh"
+export TOOL_BINECC="${BSP_BASEDIR}/tools/bin/nandbingen"
+export TOOL_MKPARAM="${BSP_BASEDIR}/tools/scripts/mk_bootparam.sh"
+export TOOL_MKUBIFS="${BSP_BASEDIR}/tools/scripts/mk_ubifs.sh"
 export TOOL_MKEXT4="make_ext4fs"
 
 # secure keys
-export SECURE_BOOTKEY="${BASEDIR}/tools/files/secure-bootkey.pem"
-export SECURE_USERKEY="${BASEDIR}/tools/files/secure-userkey.pem"
-export SECURE_BL1_ENCKEY="${BASEDIR}/tools/files/secure-bl1-enckey.txt"
-export SECURE_BL32_ENCKEY="${BASEDIR}/tools/files/secure-bl32-enckey.txt"
+export SECURE_BOOTKEY="${BSP_BASEDIR}/tools/files/secure-bootkey.pem"
+export SECURE_USERKEY="${BSP_BASEDIR}/tools/files/secure-userkey.pem"
+export SECURE_BL1_ENCKEY="${BSP_BASEDIR}/tools/files/secure-bl1-enckey.txt"
+export SECURE_BL32_ENCKEY="${BSP_BASEDIR}/tools/files/secure-bl32-enckey.txt"
 
 # b1 configs
 export BL1_BIN="bl1-nxp3220.bin"
 export BL1_LOADADDR=0xFFFF0000
-export BL1_NSIH="${BASEDIR}/tools/files/nsih_bl1.txt"
+export BL1_NSIH="${BSP_BASEDIR}/tools/files/nsih_bl1.txt"
 
 # b2 configs
 export BL2_BIN="bl2-${TARGET_BL2_BOARD}.bin"
 export BL2_LOADADDR=0xFFFF9000
-export BL2_NSIH="${BL2_DIR}/reference-nsih/$TARGET_BL2_NSIH"
+export BL2_NSIH="${BSP_BL2_DIR}/reference-nsih/$TARGET_BL2_NSIH"
 export BL2_CHIP=${TARGET_BL2_CHIP}
 export BL2_BOARD=${TARGET_BL2_BOARD}
 export BL2_PMIC=${TARGET_BL2_PMIC}
@@ -55,14 +55,14 @@ export BL2_PMIC=${TARGET_BL2_PMIC}
 # b32 configs
 export BL32_BIN="bl32.bin"
 export BL32_LOADADDR=${TARGET_BL32_LOADADDR}
-export BL32_NSIH="${BL32_DIR}/reference-nsih/nsih_general.txt"
+export BL32_NSIH="${BSP_BL32_DIR}/reference-nsih/nsih_general.txt"
 
 # uboot configs
 export UBOOT_BIN="u-boot.bin"
 export UBOOT_LOADADDR=0x43c00000
-export UBOOT_NSIH="${BASEDIR}/tools/files/nsih_uboot.txt"
+export UBOOT_NSIH="${BSP_BASEDIR}/tools/files/nsih_uboot.txt"
 export UBOOT_DEFCONFIG=${TARGET_UBOOT_DEFCONFIG}
-export UBOOT_LOGO_BMP="${BASEDIR}/tools/files/logo.bmp"
+export UBOOT_LOGO_BMP="${BSP_BASEDIR}/tools/files/logo.bmp"
 
 # kernel configs
 export KERNEL_DEFCONFIG=${TARGET_KERNEL_DEFCONFIG}
@@ -78,21 +78,21 @@ export IMAGE_BOOT_SIZE=${TARGET_BOOT_IMAGE_SIZE}
 export IMAGE_ROOT_SIZE=${TARGET_ROOT_IMAGE_SIZE}
 export IMAGE_DATA_SIZE=${TARGET_DATA_IMAGE_SIZE}
 
-export TOOL_FILES=(
-	"${BASEDIR}/tools/scripts/partmap_fastboot.sh"
-	"${BASEDIR}/tools/scripts/partmap_diskimg.sh"
-	"${BASEDIR}/tools/scripts/usb-down.sh"
-	"${BASEDIR}/tools/scripts/configs/udown.bootloader.sh"
-	"${BASEDIR}/tools/scripts/configs/udown.bootloader-secure.sh"
-	"${BASEDIR}/tools/bin/linux-usbdownloader"
-	"${BASEDIR}/tools/bin/simg2dev"
-	"${BASEDIR}/tools/files/partmap_*.txt"
-	"${BASEDIR}/tools/scripts/swu_image.sh"
-	"${BASEDIR}/tools/scripts/swu_hash.py"
-	"${BASEDIR}/tools/files/secure-bootkey.pem"
-	"${BASEDIR}/tools/files/secure-userkey.pem"
-	"${BASEDIR}/tools/files/secure-jtag-hash.txt"
-	"${BASEDIR}/tools/files/efuse_cfg-aes_enb.txt"
-	"${BASEDIR}/tools/files/efuse_cfg-verify_enb-hash0.txt"
-	"${BASEDIR}/tools/files/efuse_cfg-sjtag_enb.txt"
+export BSP_TOOL_FILES=(
+	"${BSP_BASEDIR}/tools/scripts/partmap_fastboot.sh"
+	"${BSP_BASEDIR}/tools/scripts/partmap_diskimg.sh"
+	"${BSP_BASEDIR}/tools/scripts/usb-down.sh"
+	"${BSP_BASEDIR}/tools/scripts/configs/udown.bootloader.sh"
+	"${BSP_BASEDIR}/tools/scripts/configs/udown.bootloader-secure.sh"
+	"${BSP_BASEDIR}/tools/bin/linux-usbdownloader"
+	"${BSP_BASEDIR}/tools/bin/simg2dev"
+	"${BSP_BASEDIR}/tools/files/partmap_*.txt"
+	"${BSP_BASEDIR}/tools/scripts/swu_image.sh"
+	"${BSP_BASEDIR}/tools/scripts/swu_hash.py"
+	"${BSP_BASEDIR}/tools/files/secure-bootkey.pem"
+	"${BSP_BASEDIR}/tools/files/secure-userkey.pem"
+	"${BSP_BASEDIR}/tools/files/secure-jtag-hash.txt"
+	"${BSP_BASEDIR}/tools/files/efuse_cfg-aes_enb.txt"
+	"${BSP_BASEDIR}/tools/files/efuse_cfg-verify_enb-hash0.txt"
+	"${BSP_BASEDIR}/tools/files/efuse_cfg-sjtag_enb.txt"
 )
